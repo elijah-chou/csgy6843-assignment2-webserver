24eb8c399eadf47ad80a14941f449c4c159ec6e3
Elijah Chou <ecool7079@gmail.com>
2026-01-31 16:28:27 -0500

    Add server and connection headers to HTTP response

--- webServer.py
+++ webServer.py
@@ -39,6 +39,9 @@ def webServer(port=13331):
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
 
+      # Add server and connection headers
+      outputdata += b"Server: SimplePythonServer/1.0\r\n"
+      outputdata += b"Connection: close\r\n\r\n"
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
